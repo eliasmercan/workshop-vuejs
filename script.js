d9ae41871bf49eb6de37872500a38756229f3bac
Elias Mercado Cantero <eliasmercan@msn.com>
2019-01-30 10:55:00 -0500

    Workshop terminado

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 new Vue({
     el:"#appVue",
     data:{
-        lists:[
-            {keep: 'uno', completed: true},
-            {keep: 'dos', completed: false},
-            {keep: 'tres', completed: true},
-        ],
+        lists:[],
 
         newKeep: '',
     },
