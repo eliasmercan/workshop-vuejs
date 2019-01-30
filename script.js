02e4fb407ff9cfdf2e317e52aa0b7eda7d0cbe1a
Elias Mercado Cantero <eliasmercan@msn.com>
2019-01-30 10:19:39 -0500

    Bloqueando la lista

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 new Vue({
     el:"#appVue",
     data:{
-        lists:[],
+        lists:[
+            {keep: 'uno', completed: true},
+            {keep: 'dos', completed: false},
+            {keep: 'tres', completed: true},
+        ],
+
+        newKeep: '',
+    },
+
+    methods:{
+        addkeep: function(){
+            this.lists.push({keep: this.newKeep, completed: false});
+            this.newKeep = '';
+        }
     }
 });
